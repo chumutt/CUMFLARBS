fe95f32266355f79a68903b2f570e57a1d2f2935
chu <chu@dogleash.xyz>
2023-11-01 20:35:09 -0500

    init

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/chumutt/voidrice.git"
+progsfile="https://raw.githubusercontent.com/chumutt/CUMFLARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
